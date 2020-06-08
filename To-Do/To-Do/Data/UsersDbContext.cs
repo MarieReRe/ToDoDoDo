d9734f4e0fc879bc80e4edd3891a46783e51ebc0
Marie Antons <chefantons@gmail.com>
2020-06-08 16:00:25 -0500

    added a curly to connection string

--- To-Do/To-Do/Data/UsersDbContext.cs
+++ To-Do/To-Do/Data/UsersDbContext.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using To_Do.Models.Identity;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 using To_Do.Model.Identity;
 
 namespace To_Do.Data
