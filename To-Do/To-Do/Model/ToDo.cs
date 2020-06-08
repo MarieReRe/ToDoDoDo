61c5ae73e4d450917d14f406d14e252c3966fa2e
Marie Antons <chefantons@gmail.com>
2020-06-08 14:49:25 -0500

    ToDo model created

---
+++ To-Do/To-Do/Model/ToDo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace To_Do.Model
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public DateTime? ExpectedCompletion { get; set; }
+
+        public string Assignee {get;set;} 
+
+        public int Difficulty { get; set; }
+    }
+}
