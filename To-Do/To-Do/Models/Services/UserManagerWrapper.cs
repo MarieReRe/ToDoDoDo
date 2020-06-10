09259182f467f2fddab91a7083b03e9197fd2266
Marie Antons <chefantons@gmail.com>
2020-06-09 19:20:05 -0500

    added Interface of IUserManager

--- To-Do/To-Do/Models/Services/UserManagerWrapper.cs
+++ To-Do/To-Do/Models/Services/UserManagerWrapper.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using To_Do.Models.Interfaces;
 
 namespace To_Do.Models.Services
 {
-    public class UserManagerWrapper
+    public class UserManagerWrapper : IUserManager
     {
+
     }
 }
