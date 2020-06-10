d3bec768b72b3d7abb20e816ddc03e71ae8facc1
Marie Antons <chefantons@gmail.com>
2020-06-09 19:21:57 -0500

    added the UserManager of ToDoUser to User Manager Wrapper

--- To-Do/To-Do/Models/Services/UserManagerWrapper.cs
+++ To-Do/To-Do/Models/Services/UserManagerWrapper.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using To_Do.Models.Identity;
 using To_Do.Models.Interfaces;
 
 namespace To_Do.Models.Services
 {
     public class UserManagerWrapper : IUserManager
     {
-
+        private readonly UserManager<ToDoUser> userManager;
+       
     }
 }
