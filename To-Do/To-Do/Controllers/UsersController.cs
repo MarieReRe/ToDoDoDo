e40188e05cea70e678091b67ae972a8c892dd70f
Marie Antons <chefantons@gmail.com>
2020-06-09 20:15:10 -0500

    added userManager interface to controller

--- To-Do/To-Do/Controllers/UsersController.cs
+++ To-Do/To-Do/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using To_Do.Models.Interfaces;
 
 namespace To_Do.Controllers
 {
@@ -11,6 +12,11 @@ namespace To_Do.Controllers
     [ApiController]
     public class UsersController : Controller
     {
+        private readonly IUserManager userManager;
+        public UsersController(IUserManager userManager)
+        {
+            this.userManager = userManager;
+        }
         public IActionResult Index()
         {
             return View();
