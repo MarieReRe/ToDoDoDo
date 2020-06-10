cc6d3e20a27ea39032717f24bb1b7645e8d1172b
Marie Antons <chefantons@gmail.com>
2020-06-09 19:59:17 -0500

    added updateUserData

---
+++ To-Do/To-Do/Models/Identity/UpdateUserData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace To_Do.Models.Identity
+{
+    public class UpdateUserData
+    {
+    }
+}
